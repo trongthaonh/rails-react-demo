bd564250b0abd16e52ea31a31967e5b8418e4382
Trong Thao <trongthaonh@gmail.com>
2016-01-20 16:29:20 +0700

    // Initialize the main component state to re-render itself

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,22 +1,31 @@
 import TweetBox from './components/TweetBox';
 import TweetList from './components/TweetList';
 
-let mockTweets = [
-  {id: 1, name: 'Trong Thao', body: 'My First Tweet'},
-  {id: 2, name: 'Trong Thao', body: 'My Second Tweet'},
-  {id: 3, name: 'Trong Thao', body: 'My Third Tweet'}
-];
+let mockTweets = [];
 
 class Main extends React.Component {
-  addTweet(){
+  // Initialize the main component state to re-render itself
+  constructor(props) {
+    super(props);
+    this.state = { tweetsList : mockTweets };
+  }
 
+  addTweet(tweetToAdd){
+    let newTweetsList = this.state.tweetsList;
+    newTweetsList.unshift({
+      id: Date.now(),
+      name: "Guest",
+      body: tweetToAdd
+    });
+
+    this.setState({ tweetsList : newTweetsList })
   };
 
   render() {
     return (
       <div className="container">
         <TweetBox sendTweet={this.addTweet.bind(this)}/>
-        <TweetList tweets={mockTweets}/>
+        <TweetList tweets={this.state.tweetsList}/>
       </div>
     );
   }
