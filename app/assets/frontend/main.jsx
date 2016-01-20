877f2352deffa12241c88d72f54bf92d0b32fc54
Trong Thao <trongthaonh@gmail.com>
2016-01-20 16:47:47 +0700

    Fix bug cause the React app is still trying to mount itself

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -32,10 +32,13 @@ class Main extends React.Component {
 }
 
 let documentReady = () => {
-  ReactDOM.render(
-      <Main />,
-      document.getElementById("react")
-  );
+  let reactNode = document.getElementById("react");
+  if (reactNode){
+    ReactDOM.render(
+        <Main />,
+        reactNode
+    );
+  }
 };
 
 $(documentReady);
