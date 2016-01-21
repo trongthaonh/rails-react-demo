6ba2322d91a7e5c93e7acf0dd202bd57555b9e3a
Trong Thao <trongthaonh@gmail.com>
2016-01-21 15:40:53 +0700

    Create TweetActions and communicate with API utility

---
+++ app/assets/frontend/API.jsx
@@ -0,0 +1,9 @@
+import ServerActions from "./actions/ServerActions"
+
+export default {
+  getAllTweets() {
+    $.get("/tweets")
+    .success(rawTweets => ServerActions.receivedTweets(rawTweets))
+    .error(error => console.log(error));
+  }
+}
